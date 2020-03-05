3d31a62946a001f0b71272ec5ce7adf003d0357b
Cilvin <cilvin33@gmail.com>
2020-03-04 20:22:13 -0800

    First commit

---
+++ client-2/src/App.js
@@ -0,0 +1,23 @@
+// App.js
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import moviesApp from './reducers/reducers';
+import MainView from './components/main-view/main-view';
+
+import './App.css';
+
+const store = createStore(moviesApp);
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <MainView />
+      </Provider>
+    );
+  }
+}
+
+export default App;
